9acca4a0be4a3b46fc4ab0b9d5a8139feed4559c
Matt Seccafien <mseccafien@gmail.com>
2023-06-24 02:14:11 +0200

    swagger init

---
+++ src/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ManageGlossariesApi';
+export * from './MetaInformationApi';
+export * from './TranslateDocumentsApi';
+export * from './TranslateTextApi';
